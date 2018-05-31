5ed645ee9c1a57151d0c913c30aeb3588580ed88
Auston Stewart <auston@shedbuilt.net>
2018-05-30 14:17:40 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-patch -Np1 -i "${SHED_PKG_PATCH_DIR}/ninja-1.8.2-add_NINJAJOBS_var-1.patch" || exit 1
-python3 configure.py --bootstrap || exit 1
-install -vDm755 ninja "${SHED_FAKE_ROOT}/usr/bin/ninja"
-install -vDm644 misc/ninja.vim "${SHED_FAKE_ROOT}/usr/share/vim/vim80/syntax/ninja.vim"
-install -vDm644 misc/bash-completion "${SHED_FAKE_ROOT}/usr/share/bash-completion/completions/ninja"
+# Patch
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/ninja-1.8.2-add_NINJAJOBS_var-1.patch" &&
+# Configure and Build
+python3 configure.py --bootstrap &&
+# Install
+install -vDm755 ninja "${SHED_FAKE_ROOT}/usr/bin/ninja" &&
+install -vDm644 misc/ninja.vim "${SHED_FAKE_ROOT}/usr/share/vim/vim80/syntax/ninja.vim" &&
+install -vDm644 misc/bash-completion "${SHED_FAKE_ROOT}/usr/share/bash-completion/completions/ninja" &&
 install -vDm644 misc/zsh-completion "${SHED_FAKE_ROOT}/usr/share/zsh/site-functions/_ninja"
